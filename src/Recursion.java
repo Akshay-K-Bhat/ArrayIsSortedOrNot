43adad69d6164e308582d13548e55e25e07875e2
Akshay bhat <akshaybhat126@gmail.com>
2022-07-15 21:39:44 +0530

    Initial commit

---
+++ src/Recursion.java
@@ -0,0 +1,23 @@
+
+
+public class Recursion {
+           public static boolean isSorted(int[] arr, int idx) {
+           if (idx == arr.length - 1) {
+               return true;
+           }
+
+           if (arr[idx] < arr[idx + 1]) {
+               return isSorted(arr, idx + 1);
+           } else {
+               return false;
+           }
+       }
+
+
+        public static void main(String[] args) {
+            int[] arr = {0, 1, 2, 3, 4, 5,5};
+            boolean value = isSorted(arr,0);
+            System.out.println(value);
+        }
+
+}
